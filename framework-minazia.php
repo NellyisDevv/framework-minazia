ee0aec3964c94aba119b0e21d8b1098a85436fd1
Nelson Rosario <NellyisDev@gmail.com>
2025-03-13 16:44:16 -0400

    New CSS Classes

--- framework-minazia.php
+++ framework-minazia.php
@@ -3,7 +3,7 @@
  * Plugin Name: Minazia Framework
  * Plugin URI: https://www.minazia.com
  * Description: A flexible framework for Divi.
- * Version: 1.4.2
+ * Version: 1.4.3
  * Author: Minazia CO
  * Author URI: https://www.minazia.com
  * Text Domain: minazia-framework
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const order = require('gulp-order')
 const through2 = require('through2')
 
 // Task to generate max-width classes (max-w-5 to max-w-800)
-gulp.task('generate-max-widths', function () {
+gulp.task('generate-max-width', function () {
   return gulp
     .src('css/custom-max-width.css', { allowEmpty: true })
     .pipe(
@@ -13,9 +13,49 @@ gulp.task('generate-max-widths', function () {
         let cssContent = ''
         // Generate max-w-5 through max-w-800
         for (let i = 1; i <= 120; i++) {
-          cssContent += `.max-w-${i * 10} { max-width: ${
+          cssContent += `.mx-w-${i * 10} { max-width: ${
             i * 10
           }px !important; }\n`
+        }
+        file.contents = Buffer.from(cssContent)
+        cb(null, file)
+      })
+    )
+    .pipe(gulp.dest('css'))
+})
+
+// Task to generate min-width classes (min-w-5 to min-w-800)
+gulp.task('generate-min-width', function () {
+  return gulp
+    .src('css/custom-min-width.css', { allowEmpty: true })
+    .pipe(
+      through2.obj(function (file, enc, cb) {
+        let cssContent = ''
+        // Generate min-w-5 through min-w-800
+        for (let i = 1; i <= 120; i++) {
+          cssContent += `.mn-w-${i * 10} { min-width: ${
+            i * 10
+          }px !important; }\n`
+        }
+        file.contents = Buffer.from(cssContent)
+        cb(null, file)
+      })
+    )
+    .pipe(gulp.dest('css'))
+})
+
+// Task to generate min-height classes (min-h-10 to min-h-100)
+gulp.task('generate-min-height', function () {
+  return gulp
+    .src('css/custom-min-height.css', { allowEmpty: true })
+    .pipe(
+      through2.obj(function (file, enc, cb) {
+        let cssContent = ''
+        // Generate min-h-10 through min-h-100 in 10vh increments
+        for (let i = 1; i <= 10; i++) {
+          cssContent += `.mn-h-${i * 10}vh { min-height: ${
+            i * 10
+          }vh !important; }\n`
         }
         file.contents = Buffer.from(cssContent)
         cb(null, file)
@@ -27,7 +67,7 @@ gulp.task('generate-max-widths', function () {
 // Task to build CSS
 gulp.task(
   'css',
-  gulp.series('generate-max-widths', function () {
+  gulp.series('generate-max-width', 'generate-min-height', function () {
     return gulp
       .src('css/*.css')
       .pipe(
@@ -42,6 +82,8 @@ gulp.task(
             'shadows.css',
             'border-radius.css',
             'max-width.css',
+            'custom-max-width.css', // Added generated max-width file
+            'custom-min-height.css', // Added generated min-height file
             'buttons.css',
             'flexbox.css',
             'css/*.css',
